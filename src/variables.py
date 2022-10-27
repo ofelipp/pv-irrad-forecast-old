891803c0b4f4756df59bcdb1f4e38815a0c3b9b4
ofelipp <ofelippm@gmail.com>
2022-10-26 23:11:01 -0300

    [ADD] function to evaluate possible ranges for variables

--- src/variables.py
+++ src/variables.py
@@ -3,10 +3,30 @@ Module which objective is concat every variable create on model dataset
 for training the model.
 """
 
+import json
+import numpy as np
 import pandas as pd
 
+MTR_VARS_RANGES = "src/static/metereological_variable_ranges.json"
 
-def var_season(model_data: pd.DataFrame, datetime_col: str):
+
+def read_meteorological_vars_range(json_path=MTR_VARS_RANGES) -> dict:
+
+    """JSON file with variables ranges"""
+
+    with open(json_path, mode="r", encoding="utf8") as file:
+        return json.load(file)
+
+
+def var_range(variable: str) -> pd.DataFrame:
+
+    """Dataframe containing variable's choosed range"""
+
+    v_range = read_meteorological_vars_range()[variable]
+    return pd.DataFrame(v_range).transpose()
+
+
+def season(model_data: pd.DataFrame, datetime_col: str) -> None:
     """
     Create Season variable using datetime column from pandas DataFrame
 
@@ -17,13 +37,7 @@ def var_season(model_data: pd.DataFrame, datetime_col: str):
 
     """
 
-    seasons = {
-        "summer_pre": {"start": "01-01", "end": "03-20"},
-        "autumn": {"start": "03-21", "end": "06-20"},
-        "winter": {"start": "06-21", "end": "09-22"},
-        "spring": {"start": "09-23", "end": "12-20"},
-        "summer_post": {"start": "12-21", "end": "12-31"},
-    }
+    seasons = read_meteorological_vars_range()["seasons"]
 
     # Create Season column
     model_data["Season"] = None
@@ -59,3 +73,32 @@ def var_season(model_data: pd.DataFrame, datetime_col: str):
 
     # Drop aux seasons columns
     model_data.drop(columns=["Year"], inplace=True)
+
+
+def possible_range(model_data: pd.DataFrame, var_col: str) -> np.ndarray:
+
+    """
+    Variable values evaluated in a designed range
+    """
+
+    print(f"{var_col}")
+
+    # Reading values from json file
+    df_var_range = var_range(var_col)
+
+    # Merging with original dataframe
+    df_var = pd.merge(
+        left=model_data[["Season", var_col]],
+        right=df_var_range,
+        left_on="Season",
+        right_index=True,
+        how="left",
+    )
+
+    # Conditions to exclude values
+    _below = df_var[var_col] < df_var["min"]
+    _above = df_var[var_col] > df_var["max"]
+
+    print(f"Cases Below: {_below.sum()} | Cases Above: {_above.sum()}")
+
+    return np.where(_below | _above, np.nan, df_var[var_col])
