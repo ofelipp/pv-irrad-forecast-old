ba9b1dc37ad62d8acce170e39ef619e4c8ea10f4
ofelipp <ofelippm@gmail.com>
2023-03-19 18:12:55 -0300

    [REF] flake8 adapts

--- src/data/io.py
+++ src/data/io.py
@@ -12,7 +12,7 @@ import os
 
 def json_to_dict(path: str) -> dict:
 
-    """ Function used to import json archive into python dictionary """
+    """Function used to import json archive into python dictionary"""
 
     with open(path, mode="r+", encoding="utf8") as json_file:
         return json.load(json_file)
@@ -20,18 +20,19 @@ def json_to_dict(path: str) -> dict:
 
 def dict_to_json(dictionary: dict, path: str):
 
-    """ Function used to export dictionary in json format """
+    """Function used to export dictionary in json format"""
 
     with open(path, mode="w", encoding="utf8") as json_file:
         json.dump(dictionary, json_file, indent=4)
 
-    assert os.path.isfile(path) is True, \
-        f'Impossible to save json file.. not founded in {path}'
+    assert (
+        os.path.isfile(path) is True
+    ), f"Impossible to save json file.. not founded in {path}"
 
 
 def list_dir_files(path: str) -> list:
 
-    """ Function which returns a list of filepaths in a directory """
+    """Function which returns a list of filepaths in a directory"""
 
     all_files = []
 
@@ -47,19 +48,21 @@ def list_dir_files(path: str) -> list:
 
 def load_artfact(filepath):
 
-    assert os.path.isfile(filepath) is True, \
-        f'Artfact not founded in {filepath}'
+    assert (
+        os.path.isfile(filepath) is True
+    ), f"Artfact not founded in {filepath}"
 
-    with open(filepath, 'rb') as input_model:
+    with open(filepath, "rb") as input_model:
         return pickle.load(input_model)
 
 
 def save_artfact(model, model_name: str, path: str):
 
-    _filepath = f"{path}/{model_name}.pickle"
+    _filepath = f"{path}{model_name}.pickle"
 
-    with open(_filepath, 'wb') as output_model:
+    with open(_filepath, "wb") as output_model:
         pickle.dump(model, output_model)
 
-    assert os.path.isfile(_filepath) is True, \
-        f'Impossible to save {model_name} artfact not founded in {_filepath}'
+    assert (
+        os.path.isfile(_filepath) is True
+    ), f"Impossible to save {model_name} artfact not founded in {_filepath}"
