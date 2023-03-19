1b298c77f27a5075a60c7edd1a44c9e2da1a7c5c
ofelipp <ofelippm@gmail.com>
2023-03-19 18:28:33 -0300

    [REF] flake8 adapts, correcting logs

--- src/model/linear_regression.py
+++ src/model/linear_regression.py
@@ -6,39 +6,43 @@ functions.
 """
 
 from data.io import save_artfact
+from logging import debug, info
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
 def linear_regression():
 
-    """ Creates a linear regression model """
+    """Creates a linear regression model"""
 
     return LinearRegression()
 
 
 def train_linear_regression(
-    model: LinearRegression, features: np.ndarray, labels: np.ndarray,
-    model_name: str, save_path: str
+    model: LinearRegression,
+    features: np.ndarray,
+    labels: np.ndarray,
+    model_name: str,
+    save_path: str,
 ) -> LinearRegression:
 
-    """ Train Linear Regression model to perform predictions """
+    """Train Linear Regression model to perform predictions"""
+
+    info("===== Training Linear Regression ===== ")
 
     debug(f"Shape of features:\t{features.shape}")
     debug(f"Shape of labels:\t{features.shape}")
-
-    info("Starting training...")
 
     trained_lr = model.fit(features, labels)
 
     info("Training complete...")
 
-    debug(f"R_Squared:\t{trained_lr.score(features, labels)}")
-    debug(f"Intercept:\t{trained_lr.intercept_}")
-    debug(f"Coeficients:\t{trained_lr.coef_}")
+    debug(f"\tR_Squared:\t{trained_lr.score(features, labels)}")
+    debug(f"\tIntercept:\t{trained_lr.intercept_}")
+    debug(f"\tCoeficients:\t{trained_lr.coef_}")
 
     info("Saving Model pickle...")
 
-    save_artfact(trained_lr, f"linear_regression{model_name}", save_path)
+    save_artfact(trained_lr, model_name, save_path)
 
     return trained_lr
